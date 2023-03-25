a5776aabeaabc7195af61864dc985f6124a788d0
Yankevych <pawabiker@gmail.com>
2023-03-25 18:23:23 +0200

    added one func that control all, price of plates

--- car_plates_parser.py
+++ car_plates_parser.py
@@ -65,7 +65,8 @@ def html_receive(region):
 
     return decoded_utf
 
-def handler0(region, name_of_tsc):
+def handler(region, name_of_tsc, price):
+    datetime_start = datetime.datetime.now()
     decoded_utf = html_receive(region)
     soup = BeautifulSoup(decoded_utf, 'html.parser')
 
@@ -82,7 +83,7 @@ def handler0(region, name_of_tsc):
             columns = row.find_all('td')
             # добавляем данные в список
             data.append([col.text.strip() for col in columns])
-    
+
     except:
         data.append('Nothing')
 
@@ -90,7 +91,7 @@ def handler0(region, name_of_tsc):
     df = pd.DataFrame(data[1:], columns=data[0])
 
     # фильтруем данные
-    filtered_df = df[(df.iloc[:, 1] == '0') & (
+    filtered_df = df[(df.iloc[:, 1] == f'{price}') & (
         df.iloc[:, 2].str.contains(f'{name_of_tsc}'))]
 
     # сортируем по первому столбцу в порядке от А до Я
@@ -110,203 +111,9 @@ def handler0(region, name_of_tsc):
 
     # выводим количество номеров в первом столбце
     datetime_now = datetime.datetime.now()
-    print(f'Количество номеров: {data_len}, запрос на {name_of_tsc}, {region} регион, {datetime_now}')
-
-    return data_len
-
-def handler4(region, name_of_tsc):
-    decoded_utf = html_receive(region)
-    soup = BeautifulSoup(decoded_utf, 'html.parser')
-
-    # получаем таблицу
-    table = soup.find('table')
-
-    # создаем пустой список, в который будем добавлять данные
-    data = []
-
-    # проходимся по каждой строке таблицы
-    try:
-        for row in table.find_all('tr'):
-            # получаем значения ячеек
-            columns = row.find_all('td')
-            # добавляем данные в список
-            data.append([col.text.strip() for col in columns])
-    
-    except:
-        data.append('Nothing')
-
-    # создаем DataFrame с полученными данными
-    df = pd.DataFrame(data[1:], columns=data[0])
-
-    # фильтруем данные
-    filtered_df = df[(df.iloc[:, 1] == '4000') & (
-        df.iloc[:, 2].str.contains(f'{name_of_tsc}'))]
-
-    # сортируем по первому столбцу в порядке от А до Я
-    filtered_df = filtered_df.sort_values(
-        by=filtered_df.columns[0], ascending=True)
-
-    # сохраняем данные в Excel
-    with pd.ExcelWriter(f'{name_of_tsc}.xlsx', engine='xlsxwriter') as writer:
-        filtered_df.to_excel(writer, index=False, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-
-        # задаем ширину первого столбца в соответствии с его содержимым
-        worksheet.set_column(0, 0, max(df.iloc[:, 0].str.len()) + 1)
-
-    data_len = filtered_df.shape[0]
-
-    # выводим количество номеров в первом столбце
-    datetime_now = datetime.datetime.now()
-    print(f'Количество номеров: {data_len}, запрос на {name_of_tsc}, {region} регион, {datetime_now}')
-
-    return data_len
-
-def handler8(region, name_of_tsc):
-    decoded_utf = html_receive(region)
-    soup = BeautifulSoup(decoded_utf, 'html.parser')
-
-    # получаем таблицу
-    table = soup.find('table')
-
-    # создаем пустой список, в который будем добавлять данные
-    data = []
-
-    # проходимся по каждой строке таблицы
-    try:
-        for row in table.find_all('tr'):
-            # получаем значения ячеек
-            columns = row.find_all('td')
-            # добавляем данные в список
-            data.append([col.text.strip() for col in columns])
-    
-    except:
-        data.append('Nothing')
-
-    # создаем DataFrame с полученными данными
-    df = pd.DataFrame(data[1:], columns=data[0])
-
-    # фильтруем данные
-    filtered_df = df[(df.iloc[:, 1] == '8000') & (
-        df.iloc[:, 2].str.contains(f'{name_of_tsc}'))]
-
-    # сортируем по первому столбцу в порядке от А до Я
-    filtered_df = filtered_df.sort_values(
-        by=filtered_df.columns[0], ascending=True)
-
-    # сохраняем данные в Excel
-    with pd.ExcelWriter(f'{name_of_tsc}.xlsx', engine='xlsxwriter') as writer:
-        filtered_df.to_excel(writer, index=False, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-
-        # задаем ширину первого столбца в соответствии с его содержимым
-        worksheet.set_column(0, 0, max(df.iloc[:, 0].str.len()) + 1)
-
-    data_len = filtered_df.shape[0]
-
-    # выводим количество номеров в первом столбце
-    datetime_now = datetime.datetime.now()
-    print(f'Количество номеров: {data_len}, запрос на {name_of_tsc}, {region} регион, {datetime_now}')
-
-    return data_len
-
-def handler10(region, name_of_tsc):
-    decoded_utf = html_receive(region)
-    soup = BeautifulSoup(decoded_utf, 'html.parser')
-
-    # получаем таблицу
-    table = soup.find('table')
-
-    # создаем пустой список, в который будем добавлять данные
-    data = []
-
-    # проходимся по каждой строке таблицы
-    try:
-        for row in table.find_all('tr'):
-            # получаем значения ячеек
-            columns = row.find_all('td')
-            # добавляем данные в список
-            data.append([col.text.strip() for col in columns])
-    
-    except:
-        data.append('Nothing')
-
-    # создаем DataFrame с полученными данными
-    df = pd.DataFrame(data[1:], columns=data[0])
-
-    # фильтруем данные
-    filtered_df = df[(df.iloc[:, 1] == '10000') & (
-        df.iloc[:, 2].str.contains(f'{name_of_tsc}'))]
-
-    # сортируем по первому столбцу в порядке от А до Я
-    filtered_df = filtered_df.sort_values(
-        by=filtered_df.columns[0], ascending=True)
-
-    # сохраняем данные в Excel
-    with pd.ExcelWriter(f'{name_of_tsc}.xlsx', engine='xlsxwriter') as writer:
-        filtered_df.to_excel(writer, index=False, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-
-        # задаем ширину первого столбца в соответствии с его содержимым
-        worksheet.set_column(0, 0, max(df.iloc[:, 0].str.len()) + 1)
-
-    data_len = filtered_df.shape[0]
-
-    # выводим количество номеров в первом столбце
-    datetime_now = datetime.datetime.now()
-    print(f'Количество номеров: {data_len}, запрос на {name_of_tsc}, {region} регион, {datetime_now}')
-
-    return data_len
-
-def handler30(region, name_of_tsc):
-    decoded_utf = html_receive(region)
-    soup = BeautifulSoup(decoded_utf, 'html.parser')
-
-    # получаем таблицу
-    table = soup.find('table')
-
-    # создаем пустой список, в который будем добавлять данные
-    data = []
-
-    # проходимся по каждой строке таблицы
-    try:
-        for row in table.find_all('tr'):
-            # получаем значения ячеек
-            columns = row.find_all('td')
-            # добавляем данные в список
-            data.append([col.text.strip() for col in columns])
-    
-    except:
-        data.append('Nothing')
-
-    # создаем DataFrame с полученными данными
-    df = pd.DataFrame(data[1:], columns=data[0])
-
-    # фильтруем данные
-    filtered_df = df[(df.iloc[:, 1] == '30000') & (
-        df.iloc[:, 2].str.contains(f'{name_of_tsc}'))]
-
-    # сортируем по первому столбцу в порядке от А до Я
-    filtered_df = filtered_df.sort_values(
-        by=filtered_df.columns[0], ascending=True)
-
-    # сохраняем данные в Excel
-    with pd.ExcelWriter(f'{name_of_tsc}.xlsx', engine='xlsxwriter') as writer:
-        filtered_df.to_excel(writer, index=False, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-
-        # задаем ширину первого столбца в соответствии с его содержимым
-        worksheet.set_column(0, 0, max(df.iloc[:, 0].str.len()) + 1)
-
-    data_len = filtered_df.shape[0]
-
-    # выводим количество номеров в первом столбце
-    datetime_now = datetime.datetime.now()
-    print(f'Количество номеров: {data_len}, запрос на {name_of_tsc}, {region} регион, {datetime_now}')
+    used_time = datetime_now - datetime_start
+    print(
+        f'Количество номеров: {data_len}, запрос на {name_of_tsc}, {region} регион, {datetime_now}, использовано {used_time}сек')
 
     return data_len
 
@@ -324,18 +131,28 @@ dp = Dispatcher(bot, storage=storage)
 class Region(StatesGroup):
     region = State()
 
-
 # создаем состояние для ТСЦ
+
+
 class TSC(StatesGroup):
     tsc = State()
 
-markup = InlineKeyboardMarkup().add(InlineKeyboardButton("Перевірити наявність номерних знаків ✅", callback_data="start"))
-markup_reload = InlineKeyboardMarkup().add(InlineKeyboardButton("Спробувати ще раз 🔂", callback_data="start"))
+
+class PRICE(StatesGroup):
+    price = State()
+
+
+markup = InlineKeyboardMarkup().add(InlineKeyboardButton(
+    "Перевірити наявність номерних знаків ✅", callback_data="start"))
+markup_reload = InlineKeyboardMarkup().add(
+    InlineKeyboardButton("Спробувати ще раз 🔂", callback_data="start"))
+
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     # выводим сообщение "Нажмите кнопку "начать""
     await message.answer('Привіт👋🏻 Я бот, який допоможе перевірити наявність безкоштовних номерів у твоєму сервісному центрі! Нажми на кнопку нижче 👇🏻', reply_markup=markup)
+
 
 @dp.callback_query_handler(text="start")
 async def process_start(callback_query: CallbackQuery):
@@ -382,6 +199,10 @@ async def process_region(message: types.Message, state: FSMContext):
     # сохраняем значение региона в переменную
     region = message.text
 
+    if not region.isdigit() or int(region) not in range(1,27):
+        await message.answer(f"😢 Мабуть Ви обрали неправильний регіон. \n Спробуйте обрати ще раз!\n\n ❗️Надішліть число регіону, наприклад: 26",reply_markup=markup_reload)
+        return
+
     # выводим сообщение "Напишите название или номер ТСЦ"
     await message.answer(f"Напишіть назву чи номер ТСЦ. \nЩоб дізнатись номер вашого ТСЦ, перейдіть за <a href=\"https://hsc.gov.ua/kontakti/kontakti-gsts-pidrozdiliv/\">посиланням</a>.\n❗️Надішліть номер ТСЦ, наприклад: 8046", parse_mode="HTML", disable_web_page_preview=True)
 
@@ -391,8 +212,9 @@ async def process_region(message: types.Message, state: FSMContext):
     # переходим в состояние TSC.tsc
     await TSC.tsc.set()
 
-
 # обработчик сообщения со значением названия ТСЦ
+
+
 @dp.message_handler(state=TSC.tsc)
 async def process_tsc(message: types.Message, state: FSMContext):
     # сохраняем значение названия ТСЦ в переменную
@@ -402,33 +224,76 @@ async def process_tsc(message: types.Message, state: FSMContext):
     data = await state.get_data()
     region = data.get('region')
 
+    # выводим сообщение "Укажите стоимость"
+    await message.answer(
+"""Оберiть вартiсть номерного знаку💸:
+
+Комбінації чотирьох однакових цифр — 30 тис. грн;
+Послідовні комбінації від 0001 до 0009 — 30 тис. грн;
+
+Три однакові цифри поспіль (наприклад: 0777) — 10 тис. грн;
+Дві послідовні пари однакових цифр (наприклад: 1122) — 10 тис. грн;
+Комбінації 0123 та 1234 — 10 тис. грн;
+
+Комбінації, що починаються з двох нулів, а наступні цифри неоднакові (наприклад: 0032) — 8 тис. грн;
+
+Три однакові цифри не поспіль (наприклад: 7077) — 4 тис. грн;
+Комбінації, де однакові перша і третя цифра, друга і четверта цифра; або перша й четверта цифра, друга і третя цифра (наприклад: 1221; 1212) — 4 тис. грн.""", reply_markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="0 UAH", callback_data="0"),
+             InlineKeyboardButton(text="4000 UAH", callback_data="4000"),
+             InlineKeyboardButton(text="8000 UAH", callback_data="8000"),
+             InlineKeyboardButton(text="10000 UAH", callback_data="10000"),
+             InlineKeyboardButton(text="30000 UAH", callback_data="30000"),]
+        ]
+    ))
+
+    # сохраняем значение названия ТСЦ и региона в состоянии
+    await state.update_data(tsc=tsc, region=region)
+
+    # переходим в состояние Cost.cost
+    await PRICE.price.set()
+
+# обработчик нажатия на кнопку со стоимостью
+
+
+@dp.callback_query_handler(state=PRICE.price)
+async def process_cost(callback_query: CallbackQuery, state: FSMContext):
+    # сохраняем значение стоимости в переменную
+    cost = callback_query.data
+
+    # получаем значение названия ТСЦ и региона из состояния
+    data = await state.get_data()
+    tsc = data.get('tsc')
+    region = data.get('region')
+
     # выводим сообщение с выбранными параметрами
     data_len = None
     try:
-        in_process_message = await message.answer('⏳ Перевіряю наявність…')
-        # ТУТ ХЕНДЛЕР 
-        # ТУТ ХЕНДЛЕР 
-        # ТУТ ХЕНДЛЕР 
-
-        data_len = handler4(region=region, name_of_tsc=tsc)
-        await message.answer(f"Обраний регіон: {region}\nОбраний ТСЦ: {tsc}.\nВсього вільних номерів: {data_len}")
+        # ТУТ ХЕНДЛЕР
+        # ТУТ ХЕНДЛЕР
+        # ТУТ ХЕНДЛЕР
+        in_process_message = await bot.send_message(callback_query.from_user.id, '⏳ Перевіряю наявність…')
+        data_len = handler(region=region, name_of_tsc=tsc, price=cost)
+        await bot.send_message(callback_query.from_user.id, f"Обраний регіон: {region}\nОбраний ТСЦ: {tsc}.\nВсього вільних номерів: {data_len}")
 
         file_name = f"{tsc}.xlsx"  # имя файла
-        file_path = os.path.join(os.getcwd(), file_name)  # полный путь до файла
+        # полный путь до файла
+        file_path = os.path.join(os.getcwd(), file_name)
 
         await in_process_message.delete()
         with open(file_path, "rb") as file:
             # отправляем файл в чат
-            await bot.send_document(message.chat.id, file, reply_markup=markup_reload)
+            await bot.send_document(callback_query.from_user.id, file, reply_markup=markup_reload)
             os.remove(file_name)
         # завершаем состояние и очищаем данные
         await state.finish()
-    
+
     except:
         if data_len is not None:
-            await message.answer(f"Обраний регіон: {region}\nОбраний ТСЦ: {tsc}.\nВсього вільних номерів: {data_len}, але сталася помилка. Спробуйте ще раз!")
+            await bot.send_message(callback_query.from_user.id, f"Обраний регіон: {region}\nОбраний ТСЦ: {tsc}.\nВсього вільних номерів: {data_len}, але сталася помилка. Спробуйте ще раз!")
         else:
-            await message.answer(f"😢 Сталася помилка. Спробуйте ще раз 👇🏻", reply_markup=markup_reload)
+            await bot.send_message(callback_query.from_user.id, f"😢 Сталася помилка. Спробуйте ще раз 👇🏻", reply_markup=markup_reload)
         await state.finish()
 
 
